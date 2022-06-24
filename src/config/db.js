38cdf0dacedee1768043d21194c32da71122c80d
vnemchev <vnemchev@gmail.com>
2022-06-24 23:38:41 +0200

    Good luck, Vladi!

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 exports.connectDb = () => {
-    mongoose.connect('mongodb://localhost:27017/art-gallery');
+    mongoose.connect('mongodb://localhost:27017/somerthing');
     mongoose.connection.on('open', () => console.log('Db connection established.'));
 };
