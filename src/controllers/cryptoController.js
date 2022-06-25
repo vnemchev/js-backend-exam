62695077cb587173b64eb1b719522204971aa0a6
vnemchev <vnemchev@gmail.com>
2022-06-25 09:51:45 +0200

    edit

--- src/controllers/cryptoController.js
+++ src/controllers/cryptoController.js
@@ -14,7 +14,19 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/create', (req, res) => {
+router.get('/details/:cryptoId', async (req, res) => {
+    try {
+        const crypto = await cryptoService.getOne(req.params.cryptoId).lean();
+
+        const isOwner = crypto.owner._id == req.user?._id;
+
+        res.render('crypto/details', { ...crypto, isOwner });
+    } catch (error) {
+        res.render('home/404');
+    }
+});
+
+router.get('/create', isAuthenticated, (req, res) => {
     res.render('crypto/create');
 });
 
@@ -26,6 +38,39 @@ router.post('/create', isAuthenticated, async (req, res) => {
     } catch (error) {
         res.render('crypto/create', { ...req.body, error: getErrorMessage(error) });
     }
+});
+
+router.get('/edit/:cryptoId', isAuthenticated, async (req, res) => {
+    try {
+        const crypto = await cryptoService.getOne(req.params.cryptoId).lean();
+
+        res.render('crypto/edit', { ...crypto });
+    } catch (error) {
+        res.render('crypto/edit', { error: getErrorMessage(error) });
+    }
+});
+
+router.post('/edit/:cryptoId', isAuthenticated, async (req, res) => {
+    const crypto = await cryptoService.getOne(req.params.cryptoId).lean();
+
+    try {
+        const isOwner = crypto.owner._id == req.user?._id;
+        if (!isOwner) {
+            throw {
+                message: 'Not an owner!',
+            };
+        }
+
+        await cryptoService.edit(req.params.cryptoId, req.body);
+
+        res.redirect(`/crypto/details/${crypto._id}`);
+    } catch (error) {
+        res.render('crypto/edit', { ...crypto, error: getErrorMessage(error) });
+    }
+});
+
+router.get('/delete/:cryptoId', isAuthenticated, (req, res) => {
+    res.render('crypto/create');
 });
 
 module.exports = router;
--- src/services/cryptoService.js
+++ src/services/cryptoService.js
@@ -3,3 +3,7 @@ const Crypto = require('../models/Crypto');
 exports.create = data => Crypto.create(data);
 
 exports.getAll = () => Crypto.find();
+
+exports.getOne = cryptoId => Crypto.findById(cryptoId);
+
+exports.edit = (cryptoId, data) => Crypto.findByIdAndUpdate(cryptoId, data, { runValidators: true });
